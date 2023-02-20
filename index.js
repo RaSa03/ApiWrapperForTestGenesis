2f5a1c259534a607606ce11de7df191d68903740
mac <mac@MacBook-Pro-mac.local>
2023-02-20 19:04:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const API_TOKEN = process.env.access_token;
+const PORT = process.env.PORT || 2107;
+const DOMAIN = process.env.base_domain;
+const link = `https://${DOMAIN}/api/v4/`;
+
+const postReq = async (type) => {
+  type = type.slice(5);
+  const TYPES = {
+    Сделка: "leads",
+    Контакт: "contacts",
+    Компания: "companies",
+  };
+  console.log(type, TYPES[type]);
+  const reqValue = TYPES[type];
+
+  return await fetch(link + reqValue, {
+    method: "POST",
+    body: JSON.stringify([{}]),
+    headers: {
+      Authorization: API_TOKEN,
+    },
+  }).then((response) => response.json());
+};
+
+app.use((req, res, next) => {
+  const CURRENT_URL = ["http://localhost:3005"];
+  const ACAO = req.headers.origin;
+
+  if (CURRENT_URL.includes(ACAO)) {
+    res.setHeader("Access-Control-Allow-Origin", ACAO);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  return next();
+});
+
+app.get("/api/:type", (req, res) => {
+  postReq(req.params.type).then((json) => {
+    res.send(json);
+  });
+});
+
+app.listen(PORT, () => console.log("SERVER STARTED IN PORT :" + PORT));
